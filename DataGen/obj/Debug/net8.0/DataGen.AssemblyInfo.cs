ef9b0450b5287f9cb079fab8671f8cdb3674955a
chow0n2 <64579021-chow0n2@users.noreply.replit.com>
2026-08-30 14:35:03 +0000

    Git commit prior to merge

--- DataGen/obj/Debug/net8.0/DataGen.AssemblyInfo.cs
+++ DataGen/obj/Debug/net8.0/DataGen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318ee651a6a1b43f277511cea197ed4e49d11aba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3383d7bedb8165a278886f5473bf9ff4ea2f4ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
